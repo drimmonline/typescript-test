7481c381f83764d82314e8c71967524a3931775f
Puttipong Runtakit <puttipong.runtakit@gmail.com>
2023-05-24 13:00:28 +0700

    first commit

---
+++ clock_angle.js
@@ -0,0 +1,19 @@
+"use strict";
+function calculateClockAngle(time) {
+    const [hour, minute] = time.split(':').map(Number);
+    // Calculate the angles for the hour and minute hands
+    const hourAngle = (hour % 12) * 30 + minute * 0.5;
+    const minuteAngle = minute * 6;
+    // Calculate the absolute difference between the angles
+    let angle = Math.abs(hourAngle - minuteAngle);
+    // Consider the shorter angle (360 degrees - angle) if it's greater
+    if (angle > 180) {
+        angle = 360 - angle;
+    }
+    return angle;
+}
+// Example usage:
+const inputTime = "17:30";
+const angle = calculateClockAngle(inputTime);
+console.log(`Time: ${inputTime}`);
+console.log(angle);
